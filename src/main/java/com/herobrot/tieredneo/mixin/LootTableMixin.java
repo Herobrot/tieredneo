673a3153c86e3e80959c7184bffaeb73a71590ec
Herobroto <herobrotelpro@gmail.com>
2026-08-26 02:13:58 -0600

    fixed #1

--- src/main/java/com/herobrot/tieredneo/mixin/LootTableMixin.java
+++ src/main/java/com/herobrot/tieredneo/mixin/LootTableMixin.java
@@ -1,10 +1,10 @@
 package com.herobrot.tieredneo.mixin;
 
 import com.herobrot.tieredneo.api.ModifierUtils;
-import com.herobrot.tieredneo.config.ConfigInit;
+import com.herobrot.tieredneo.init.ConfigInit;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,10 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LootTable.class)
 public class LootTableMixin {
-    @Inject(method =
-            "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootParams;)" + "Lit/unimi/dsi/fastutil" +
-                    "/objects/ObjectArrayList;", at = @At("RETURN"))
-    private void onGetRandomItems(LootParams params, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir) {
+
+    @Inject(method = "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootContext;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;", at = @At("RETURN"))
+    private void onGetRandomItems(LootContext context, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir) {
         if (ConfigInit.CONFIG.lootContainerModifier) {
             ObjectArrayList<ItemStack> generatedLoot = cir.getReturnValue();
 
